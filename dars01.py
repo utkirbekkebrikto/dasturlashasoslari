d22d05163f1734fa19abcfa9ae20785fc7c1e1cb
utkirbekkebrikto <utkiriyinfo@gmail.com>
2025-10-22 16:36:47 +0500

    dars01
    print and variables

---
+++ dars01.py
@@ -0,0 +1,56 @@
+# print("hello world")
+# print("o'tkirbek")
+# print('O\'tkirbek')
+# a=1
+# b=2
+# c=3
+# print(a,b,c)
+# print(f"a,b,c={a},{b},{c}")
+# print(f"a={a},b={b},c={c}")
+# print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+# 1 5 ning 4-darajasi
+# print("5 ning 4-darajasi", 5 ** 4)
+# a=4
+# print("uchburcchakning birinchi tamoni a=", a)
+
+# 2 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# print("22 ni 4 ga bo'lganda qancha qoldiq", 22 % 4)
+# 3 Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# print("Tomonlari 125 ga teng kvadratning yuzi",
+#       125 * 125,
+#       "ga, perimetri",
+#       4 * 125,
+#       "teng",
+# )
+# 4 Diametri 12 ga teng bo'lgan doiraning yuzini toping
+# print("Diametri 12 ga teng bo'lgan doiraning yuzi", 3.14 * (12 / 2) ** 2, "ga teng")
+# 5 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+# print(
+#     "Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi",
+#     (6 ** 2 + 7 ** 2) ** (1 / 2),
+# )
+
+# ism="Utkirbek"
+# familya="Qurbonov"
+# yosh=30
+# ty=1996
+# manzil="Zarbdor tumani"
+# print(f"ism: {ism} familya: {familya} yosh: {yosh} ty: {ty} manzil: {manzil}`")
+# print(f"ism: {ism} \n familya: {familya}\n yosh: {yosh} \n ty: {ty} manzil: {manzil}`")
+
+# Variables
+# olinmaydi
+# 133guruh="sdhfsdhfgdsfh,fsdhufhsdfjkg.djasifh"
+# ism familya="Utkirbwek Qurbonov"
+# ism-familya="utkirbek qurbonov"
+# olinadi
+# guruh133="casghdafhbAJKGH"
+# ism_familya="Utkirbek Qurbonov"
+# ismFamilya="Utkirbek Qurbonov"
+# a,b,c=1,2,4
+# a=1
+# b=2
+# c=4
+# ism,famiya,ochestva="Utkirbek Qurbonov Sulaymonovich"
+a,b,c,d,k=1,2,3,4,5
+print(f"a={a},b={b},c={c},d={d},k={k}")
